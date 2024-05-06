847bc6dbd432ce3d2e4b00e505e760d4e236c088
JorgeJCR <130024254+JorgeJCR@users.noreply.github.com>
2024-05-05 23:22:42 -0400

    Add files via upload

--- config/obtener_usuario_modificar.php
+++ config/obtener_usuario_modificar.php
@@ -21,13 +21,22 @@ $apellidoUsuario = $row['apellido'];
 return $apellidoUsuario;
 }
 
-function obtener_correo($ci_new){
+function obtener_carrera($ci_new){
     $query = "SELECT * FROM usuarios WHERE ci = '$ci_new'";
     $conexion= new mysqli('localhost','root','','reservasumss1');
     $result = $conexion->query($query);
     
     $row = $result->fetch_assoc();
-    $correoUsuario = $row['correo'];
-    return $correoUsuario;
+    $carreraUsuario = $row['carrera'];
+    return $carreraUsuario;
+}
+function obtener_materia($ci_new){
+    $query = "SELECT * FROM usuarios WHERE ci = '$ci_new'";
+    $conexion= new mysqli('localhost','root','','reservasumss1');
+    $result = $conexion->query($query);
+    
+    $row = $result->fetch_assoc();
+    $materiaUsuario = $row['materias'];
+    return $materiaUsuario;
 }
 ?>
