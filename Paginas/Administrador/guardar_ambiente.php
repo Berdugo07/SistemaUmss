fb7c4fe94faf86325d642f489db190763a6d3319
JHANINASUAREZ <129642373+JHANINASUAREZ@users.noreply.github.com>
2024-05-19 21:50:36 -0400

    Update guardar_ambiente.php

--- Paginas/Administrador/guardar_ambiente.php
+++ Paginas/Administrador/guardar_ambiente.php
@@ -15,40 +15,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $capacidad = isset($_POST['capacidad']) ? strtoupper($_POST['capacidad']) : '';
     $ubicacion = isset($_POST['ubicacion']) ? strtoupper($_POST['ubicacion']) : '';
     $piso = isset($_POST['piso']) ? strtoupper($_POST['piso']) : '';
-    $fecha = isset($_POST['fecha']) ? strtoupper($_POST['fecha']) : '';
     $tipo = isset($_POST['tipo']) ? strtoupper($_POST['tipo']) : '';
-    $img_Ambiente = isset($_POST['img_Ambiente']) ? strtoupper($_POST['img_Ambiente']) : '';
     $estado = isset($_POST['estado']) ? strtoupper($_POST['estado']) : 'HABILITADO';
 
-   // Verificar si se subió correctamente el archivo de imagen
-if (isset($_FILES["img_Ambiente"]) && $_FILES["img_Ambiente"]["error"] === 0) {
-    // Leer el contenido binario de la imagen
-    $imgContenido = file_get_contents($_FILES["img_Ambiente"]["tmp_name"]);
-
-    // Convertir el contenido binario a una cadena base64
-    $imgBase64 = base64_encode($imgContenido);
-
-    // Insertar los datos en la tabla 'ambiente'
-    $sqlAmbiente = "INSERT INTO ambiente (nombre, capacidad, ubicacion, piso, fecha, tipo, img_Ambiente, estado)
-                    VALUES ('$nombre', '$capacidad', '$ubicacion', '$piso', '$fecha', '$tipo', '$img_Ambiente', '$estado')";
-
-    if ($conn->query($sqlAmbiente) === TRUE) {
-        echo "Registro insertado correctamente";
-        // Redirigir al usuario después de la inserción exitosa
-        header("Location: RegistrodeAmbiente.php?registro=exitoso");
-        exit; // Terminar la ejecución del script después de la redirección
-    } else {
-        echo "Error al insertar datos: " . $conn->error;
+    $img_Ambiente = '';
+    // Verificar si se subió correctamente el archivo de imagen
+    if (!empty($_FILES["img_Ambiente"]) && $_FILES["img_Ambiente"]["error"] === 0) {
+        // Leer el contenido binario de la imagen
+        $imgContenido = file_get_contents($_FILES["img_Ambiente"]["tmp_name"]);
+        // Convertir el contenido binario a una cadena base64
+        $imgBase64 = base64_encode($imgContenido);
+        // Obtener la extensión del archivo de imagen
+        $img_Ambiente = $_FILES["img_Ambiente"]["name"];
     }
-} else {
-    echo "Error: no se pudo subir el archivo de imagen.";
-    exit; // Terminar la ejecución del script si hay un error
-}
-
 
     // Insertar los datos en la tabla 'ambiente'
-    $sqlAmbiente = "INSERT INTO ambiente (nombre, capacidad, ubicacion, piso, fecha, tipo, img_Ambiente, estado)
-                    VALUES ('$nombre', '$capacidad', '$ubicacion', '$piso', '$fecha', '$tipo', '$img_ambiente', '$estado')";
+    $sqlAmbiente = "INSERT INTO ambiente (nombre, capacidad, ubicacion, piso, tipo, img_Ambiente, estado)
+                    VALUES ('$nombre', '$capacidad', '$ubicacion', '$piso','$tipo', '$img_Ambiente', '$estado')";
 
     if ($conn->query($sqlAmbiente) === TRUE) {
         echo "Registro insertado correctamente";
@@ -65,3 +48,4 @@ if (isset($_FILES["img_Ambiente"]) && $_FILES["img_Ambiente"]["error"] === 0) {
     echo "Error: el formulario no se ha enviado correctamente.";
 }
 ?>
+
